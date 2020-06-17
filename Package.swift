c027033081b2500f4cc0e3267071aa82b054a066
Fan Jiang <i@fanjiang.me>
2020-06-17 19:34:58 -0400

    Fix compile

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KrakenKit",
+    platforms: [.macOS(.v10_14)],
     products: [
         .library(name: "Proto", type: .static, targets: ["Proto"]),
         .library(name: "KrakenKit", type: .static, targets: ["KrakenKit"])
